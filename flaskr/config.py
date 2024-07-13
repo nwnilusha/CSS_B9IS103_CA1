20bf8ca705312925658fac06ef6395a19fbab002
Nishshanka Sirisena <nishshanka123@gmail.com>
2024-07-13 22:12:30 +0100

    configuratoin for adding mail server for sending the email

--- flaskr/config.py
+++ flaskr/config.py
@@ -2,7 +2,10 @@
 # This file used to overide the default configurations.
 # Add all your configurations to here.
 
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 class Config:
     SECRET_KEY = "ABCD1234"
@@ -14,11 +17,13 @@ class Config:
     GOOGLE_CLIENT_SECRET = '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'
 
     # mail server configuration
-    '''MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = ('Your Name', 'your-email@gmail.com')'''
+    #MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    #MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_USERNAME = "gobuzzchat@gmail.com"
+    MAIL_PASSWORD = "rluv cdwn crxv nahm"
+    MAIL_DEFAULT_SENDER = ('Go Buzz', 'gobuzzchat@gmail.com')
 
-
+    
