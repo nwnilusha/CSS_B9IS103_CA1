cad0ab07e4c079461189c90365601dfbc971dde3
Nishshanka Sirisena <nishshanka123@gmail.com>
2024-07-18 10:01:04 +0100

    UI enhancements

--- flaskr/config.py
+++ flaskr/config.py
@@ -5,10 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    MYSQL_USER = os.getenv('MYSQL_USER')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
-    MYSQL_HOST = os.getenv('MYSQL_HOST')
-    MYSQL_DB = os.getenv('MYSQL_DB')
+    #SECRET_KEY = os.getenv('SECRET_KEY')
+    #MYSQL_USER = os.getenv('MYSQL_USER')
+    #MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+    #MYSQL_HOST = os.getenv('MYSQL_HOST')
+    #MYSQL_DB = os.getenv('MYSQL_DB')
     GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
     GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
+    SECRET_KEY = "ABCD1234"
+    MYSQL_USER = "dbs"
+    MYSQL_PASSWORD = "password"
+    MYSQL_HOST = "localhost"
+    MYSQL_DB = "GOBUZZ"
