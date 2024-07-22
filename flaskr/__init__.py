2d01ab824341fc601120a2697fff2d193ea223d7
Nishshanka Sirisena <nishshanka123@gmail.com>
2024-07-22 22:46:28 +0100

    modify to compatible with heroku

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -461,6 +461,8 @@ def create_app():
 
     return app
 
-if __name__ == "__main__":
-    app = create_app()
-    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
+
+app = create_app()
+#if __name__ == "__main__":
+#    app = create_app()
+#    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
